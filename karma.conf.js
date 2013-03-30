dbccb3e2319566e1a55670fdca1ebbf3bb1862b0
J. Leon <jleon@server>
2013-03-30 16:54:22 -0500

    Testing appears to be working

--- karma.conf.js
+++ karma.conf.js
@@ -1,49 +1,42 @@
 // Karma configuration
-// Generated on Thu Mar 28 2013 22:17:48 GMT-0500 (Central Daylight Time)
-
 
 // base path, that will be used to resolve files and exclude
-basePath = 'D:\Scribden';
-
+basePath = '';
 
 // list of files / patterns to load in the browser
 files = [
   JASMINE,
-  JASMINE_ADAPTER
+  JASMINE_ADAPTER,
+  'app/components/angular/angular.js',
+  'app/components/angular-mocks/angular-mocks.js',
+  'app/scripts/*.js',
+  'app/scripts/**/*.js',
+  'test/mock/**/*.js',
+  'test/spec/**/*.js'
 ];
-
 
 // list of files to exclude
-exclude = [
-  
-];
-
+exclude = [];
 
 // test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
+// possible values: dots || progress || growl
 reporters = ['progress'];
 
-
 // web server port
-port = 9000;
-
+port = 8080;
 
 // cli runner port
 runnerPort = 9100;
 
-
 // enable / disable colors in the output (reporters and logs)
 colors = true;
-
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
 logLevel = LOG_INFO;
 
-
 // enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
+autoWatch = false;
 
 // Start these browsers, currently available:
 // - Chrome
@@ -53,12 +46,10 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome', 'ChromeCanary', 'Firefox', 'IE', 'PhantomJS'];
-
+browsers = ['Chrome'];
 
 // If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
+captureTimeout = 5000;
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
